1b0f993e97b2d70a351db771171345e32db24db5
Jacob Morrison <jmorrison@psesd.org>
2013-10-21 12:42:44 -0700

    starting blame

--- library/infinite/db/behaviors/Blame.php
+++ library/infinite/db/behaviors/Blame.php
@@ -10,7 +10,13 @@
 namespace infinite\db\behaviors;
 
 class Blame extends \infinite\db\behaviors\ActiveRecord {
-
+	public function events()
+	{
+		return [
+			\infinite\db\ActiveQuery::EVENT_BEFORE_INSERT => 'beforeSave',
+			\infinite\db\ActiveQuery::EVENT_BEFORE_UPDATE => 'beforeSave',
+		];
+	}
 }
 
 
