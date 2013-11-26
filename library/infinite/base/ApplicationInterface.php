61a5c03edfe1943c37505436c06fe1a084845429
Jacob Morrison <jmorrison@psesd.org>
2013-11-25 17:11:28 -0800

    minor updates

---
+++ library/infinite/base/ApplicationInterface.php
@@ -0,0 +1,9 @@
+<?php
+namespace infinite\base;
+
+interface ApplicationInterface {
+	public function registerModelAlias($alias, $namespace);
+	public function getModelAliases();
+	public function registerMigrationAlias($alias);
+	public function getMigrationAliases();
+}
