f92fed0a0cabf6ff0f2ef810be07712add9edee1
Jacob Morrison <jmorrison@psesd.org>
2013-11-29 23:58:21 -0800

    with updates to main class

---
+++ library/infinite/ICD.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * library/ICD.php
+ *
+ * @author Jacob Morrison <jacob@infinitecascade.com>
+ * @package infinite
+ */
+
+class ICD
+{
+    public static function ICD($var) {
+
+    }
+}
