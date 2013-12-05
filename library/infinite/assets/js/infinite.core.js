b398f357306443ffe0d950ad43d703166fb89034
Jacob Morrison <jmorrison@psesd.org>
2013-12-05 11:08:33 -0800

    dropdown fixes

--- library/infinite/assets/js/infinite.core.js
+++ library/infinite/assets/js/infinite.core.js
@@ -1,2 +1,33 @@
 var $preparer = $.Callbacks("memory");
 $preparer.fire(document);
+
+$(window).resize(function () {
+    waitForFinalEvent(function(){
+    	$(this).trigger("resizeDone");
+    }, 250, "window-resize-event");
+});
+
+$preparer.add(function(context) {
+	$("[data-parent-height-watch]", context).each(function() {
+		var $this = $(this);
+		var offset = 0;
+		if ($this.data('parent-height-watch-offset')) {
+			offset = parseInt($this.data('parent-height-watch-offset'), 10);
+		}
+		if ($this.data('parent-height-watch-ancestor')) {
+			var $parent = $(this).parents($this.data('parent-height-watch-ancestor')).first();
+		} else {
+			var $parent = $this.parent();
+		}
+		$parent.css({display: 'block'});
+		console.log($parent);
+		var fixParent = function() {
+			if ($parent.innerHeight() < $this.outerHeight()) {
+				var newHeight = parseInt($this.outerHeight(), 10) + offset;
+				$parent.height(newHeight);
+			}
+		};
+		$(window).bind("resizeDone", fixParent);
+		fixParent();
+	});
+});
--- library/infinite/assets/js/infinite.utils.js
+++ library/infinite/assets/js/infinite.utils.js
@@ -1,3 +1,16 @@
+var waitForFinalEvent = (function () {
+  var timers = {};
+  return function (callback, ms, uniqueId) {
+    if (!uniqueId) {
+      uniqueId = "Don't call this twice without a uniqueId";
+    }
+    if (timers[uniqueId]) {
+      clearTimeout (timers[uniqueId]);
+    }
+    timers[uniqueId] = setTimeout(callback, ms);
+  };
+})();
+
 $.fn.renderSelect = function(options, blank) {
     var $this = $(this);
     $this.html('');
